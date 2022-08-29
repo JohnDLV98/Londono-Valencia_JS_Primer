9796c79c4bb7ea1f5690b7967f6e26df0dd2c7cb
JOHN DAVID LONDOÑO VALENCIA <jdlondono100171@umanizales.edu.co>
2022-08-29 18:17:08 -0500

    agregando html y css

--- js/funciones.js
+++ js/funciones.js
@@ -1,54 +1,54 @@
-let contador = 0
+// let contador = 0
 
-function facturar(){
-let continuar = true
+// function facturar(){
+// let continuar = true
 
-    do{
-        let nombre = prompt("Ingrese nombre del producto: ")
-        let valor = parseInt(prompt("Ingrese valor del producto: "))
-        imprimirProducto(nombre, valor)
-        contador += valor
+//     do{
+//         let nombre = prompt("Ingrese nombre del producto: ")
+//         let valor = parseInt(prompt("Ingrese valor del producto: "))
+//         imprimirProducto(nombre, valor)
+//         contador += valor
 
-        continuar = confirm("Desea agregar otro producto?")
+//         continuar = confirm("Desea agregar otro producto?")
 
-    }while(continuar)
+//     }while(continuar)
    
-        contadorTotal(contador)
-}
+//         contadorTotal(contador)
+// }
     
-function contadorTotal(n){
-    console.log("El total a pagar es de: "+n)
-}
+// function contadorTotal(n){
+//     console.log("El total a pagar es de: "+n)
+// }
 
-let contDomi = 0
+// let contDomi = 0
 
-function cuentaDomi(){
-    let nombreDomi = prompt("Ingrese nombre del domiciliario: ")
-    let cantDomi = parseInt( prompt("Ingrese cantidad de domicilios: "))
-    let cantHora = parseInt( prompt("Ingrese cantidad de Horas trabajadas: "))
-    let cantFact = parseInt( prompt("Ingrese monto total de compras hechas por el domiciliario: "))
+// function cuentaDomi(){
+//     let nombreDomi = prompt("Ingrese nombre del domiciliario: ")
+//     let cantDomi = parseInt( prompt("Ingrese cantidad de domicilios: "))
+//     let cantHora = parseInt( prompt("Ingrese cantidad de Horas trabajadas: "))
+//     let cantFact = parseInt( prompt("Ingrese monto total de compras hechas por el domiciliario: "))
     
-    for(let i = 0; i < cantDomi; i++){
-        let j = i+1
-        let valor = parseInt( prompt("Ingrese valor del domicilio #: "+ j))
-        contDomi += valor
-    }
+//     for(let i = 0; i < cantDomi; i++){
+//         let j = i+1
+//         let valor = parseInt( prompt("Ingrese valor del domicilio #: "+ j))
+//         contDomi += valor
+//     }
 
-    let total = contDomi - (cantHora * 4000) - cantFact
+//     let total = contDomi - (cantHora * 4000) - cantFact
 
-    if (total < 0){
-        console.log("Se le debe al domiciliario: [" + nombreDomi + "]    un total de: $" + total)
-    }
-        else if(total > 0){
-            console.log("El domiciliario: [" + nombreDomi + "]    deberá entregar: $" + total)
-    }
-    else if (total === 0) {
-        console.log("la cuenta dio $0")
-    }
+//     if (total < 0){
+//         console.log("Se le debe al domiciliario: [" + nombreDomi + "]    un total de: $" + total)
+//     }
+//         else if(total > 0){
+//             console.log("El domiciliario: [" + nombreDomi + "]    deberá entregar: $" + total)
+//     }
+//     else if (total === 0) {
+//         console.log("la cuenta dio $0")
+//     }
 
-}
+// }
 
-function imprimirProducto(a, b){
-    console.log("-- " + a + " -- Valor: " +b)
-}
+// function imprimirProducto(a, b){
+//     console.log("-- " + a + " -- Valor: " +b)
+// }
 
--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 
 
-let continuar = true
-let cont = 0
+// let continuar = true
+// let cont = 0
 
-debugger
 
-do{
 
-let ap = parseInt(prompt("elija una opcion: ||  1 --> Facturar  ||  2 --> Total a Pagar del Domiciliario  ||  "))
+// do{
 
-switch(ap){
-    case 1:
-        facturar()
-    break;
+// let ap = parseInt(prompt("elija una opcion: ||  1 --> Facturar  ||  2 --> Total a Pagar del Domiciliario  ||  "))
+
+// switch(ap){
+//     case 1:
+//         facturar()
+//     break;
     
-    case 2:
-        cuentaDomi()
-    break;
+//     case 2:
+//         cuentaDomi()
+//     break;
 
-    default:
-        console.warn("Opcion incorrecta")
+//     default:
+//         console.warn("Opcion incorrecta")
         
-}
+// }
 
-continuar = confirm("Desea continuar?")
+// continuar = confirm("Desea continuar?")
 
-}while(continuar)
+// }while(continuar)
